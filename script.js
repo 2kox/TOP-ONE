8b680f4ca97a93a75bd79a143a18a8823b9cdbac
2kox <Ali.Mo7sen8@gmail.com>
2026-05-25 10:21:57 +0300

    Update script.js

--- script.js
+++ script.js
@@ -69,9 +69,5 @@ const themeBtn = document.getElementById("themeBtn");
 themeBtn.addEventListener("click", () => {
   document.body.classList.toggle("dark");
 
-  if(document.body.classList.contains("dark")){
-    themeBtn.textContent = "☀";
-  }else{
-    themeBtn.textContent = "☾";
-  }
+  themeBtn.textContent = document.body.classList.contains("dark") ? "☀" : "☾";
 });
